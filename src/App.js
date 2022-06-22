6e0770378434a1a89d95299a473151be2b10aa77
Duy Nguyễn <duynguyen@Duys-MacBook-Pro-2.local>
2022-06-22 21:05:22 +0700

    creat router for thumb and logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
+
+//Routing
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header';
 import Home  from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 import { GlobalStyle } from './GlobalStyle' ;
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
+const App = () =>  (
+    <Router>
+         <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />}/>
+    </Routes>
+   
       <GlobalStyle />
-    </div>
+      </Router>
   );
-}
 
 export default App;

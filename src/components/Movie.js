91def5cd12c89cb870260097e9faec800ab27e1f
Duy Nguyễn <duynguyen@Duys-MacBook-Pro-2.local>
2022-06-22 22:20:44 +0700

    get movie info from api

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+//Config
 
-const Movie = () => <div>Movie</div>
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+//components
+
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
+import NoImage from "../images/no_image.jpg"
+
+const Movie = () => {
+    const{ movieId }= useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId)
+    console.log(movie);
+    return(
+        <>
+            <div>Movie</div>
+        </>
+    )
+}
 
 export default Movie

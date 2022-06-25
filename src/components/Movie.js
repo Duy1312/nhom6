13221681e56de8dcc911ddb408e2877d70fcb473
Duy Nguyễn <duynguyen@Duys-MacBook-Pro-2.local>
2022-06-25 15:17:00 +0700

    create actor in this movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -17,20 +17,34 @@ import MovieInfo from './MovieInfo'
 import NoImage from "../images/no_image.jpg"
 
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 const Movie = () => {
     const{ movieId }= useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId)
 
     if (loading) return <Spinner />
-    if(error) return<div>Somthing wrong</div>
+    if(error) return<div>Something wrong</div>
     return(
         <>
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
             <MovieInfoBar time={movie.runtime} 
-                          budget={movie.budget} 
+                          budget={movie.budget}  
                           revenue={movie.revenue} />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor 
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path ?
+                             ` ${ IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
+                        } />
+                ))}
+            </Grid>
+
         </>
     )
 }
